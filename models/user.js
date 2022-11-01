aa92f344b307673b44d04152faf797061420edba
sangyun lee <sororiri@gmail.com>
2022-11-01 17:29:08 +0900

    [justcode 이상윤] App 과 Server 분리 - 기존에 server.js 에 있던 App , Server 기능을 두개의 파일(app.js, server.js)로 분리함. - models 에 index.js 를 추가 및 typeorm 적용

--- models/user.js
+++ models/user.js
@@ -1,5 +1,19 @@
+const dataSource = require('./database');
 
-function createUser() {}
+// 구현이 되면 삭제합니다.
+async function createUser() {
+  const [userInfo] = await dataSource.query(`
+  SELECT
+    id,
+    password
+  FROM users
+  WHERE
+  email = ?
+  `,
+    ['coconut@gmail.com']
+  );
+  return userInfo;
+}
 
 function readUserByEmail(email) {}
 
