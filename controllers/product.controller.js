ef63de7bc3440e4f98ecb98e123337c9cc3b03c5
kihyeonKim <kidone0033@gmail.com>
2022-11-04 22:52:36 +0900

    로그인, 회원가입 구현

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,13 +2,13 @@ const productServ = require('../services/product.service');
 const myUtil = require('../utils/myutils.js');
 
 async function createProduct(req, res) {
-  const {title, img_url, description, category, price, properties} = req.body;
+  const { title, img_url, description, category, price, properties } = req.body;
 
-  const product = {title, img_url, description, category, price, properties};
+  const product = { title, img_url, description, category, price, properties };
   myUtil.checkDataIsNotEmpty(product);
   console.log(`req.body :`, req.body);
   await productServ.createProduct(product);
-  res.status(201).json("created product");
+  res.status(201).json('created product');
 }
 
 async function test(req, res) {
---
+++ models/userDAO.js
@@ -0,0 +1,30 @@
+const dataSource = require('./database');
+
+async function createUser(first_name, last_name, email, hashed_password) {
+  const data = await dataSource.query(`
+    INSERT INTO users (first_name, last_name, email, password)
+    VALUES
+    ('${first_name}', '${last_name}', '${email}', '${hashed_password}')
+  `);
+}
+
+async function existUser(email) {
+  const [user] = await dataSource.query(`
+    SELECT id, email from users where email = '${email}'
+  `);
+  return user;
+}
+
+async function findUserByEmail(email) {
+  const [userInfo] = await dataSource.query(`
+    SELECT id, first_name, last_name, email, password FROM users WHERE email = '${email}'
+  `);
+  return userInfo;
+}
+
+module.exports = {
+  findUserByEmail,
+  existUser,
+  createUser,
+  findUserByEmail,
+};
