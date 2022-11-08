fc7e80075dadcbc95a50d75171306cb43dc23a35
sangyun lee <sororiri@gmail.com>
2022-11-08 10:26:44 +0900

    [justcode 이상윤] Merge 중 발생한 이슈 수정

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,7 +6,5 @@ const router = express.Router();
 router.post('/', asyncWrap(productCtl.createProduct));
 router.get('/', asyncWrap(productCtl.getAllProduct));
 router.get('/test', asyncWrap(productCtl.test));
-router.get('', asyncWrap(productCtl.findProductByCategory));
-router.get('/:Id', asyncWrap(productCtl.search));
 
 module.exports = router;
