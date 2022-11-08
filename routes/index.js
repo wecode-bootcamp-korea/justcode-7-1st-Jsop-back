99e884df82455f65f5c7a6ba95cf1b67ae360ff1
SangYunLeee <35091494+SangYunLeee@users.noreply.github.com>
2022-11-08 10:20:18 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const middleware = require('../middlewares/middleware');
 const userRouter = require('./user');
 const productRouter = require('./product.route');
-const filterRouter = require('./filter');
 const categoryRouter = require('./category.route');
 const orderRouter = require('./order.route');
 
 const router = express.Router();
 
 router.use(userRouter);
-router.use(filterRouter);
 router.use('/products', productRouter);
 router.use('/orders', orderRouter);
 router.use('/categories', categoryRouter);
